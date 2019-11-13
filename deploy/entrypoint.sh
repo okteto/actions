63adb2dd2d47b1348dd9ea4cf5c4eadb9fafc9df
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-11-12 23:18:55 -0800

    dockerize deploy

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+namespace=$1
+manifest=$2
+image=$3
+tag=$4
+
+if [ ! -f kustomization.yaml ]; then 
+cat << EOF > kustomization.yaml
+resources:
+ - $manifest
+images:
+ - name: $image
+ - newName: $image
+ - newTag: $tag
+EOF
+cat kustomization.yaml
+fi
+
+kubectl apply -k ./ --namespace namespace
--- namespace/entrypoint.sh
+++ namespace/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 token=$1
 namespace=$2
-fild=$3
-path=$4
 
 export OKTETO_TOKEN=$token
 okteto version
