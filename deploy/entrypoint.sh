ead94eebc458d04da3df30d5becead34db1a0be7
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-11-12 23:31:06 -0800

    wait on

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -5,6 +5,7 @@ namespace=$1
 manifest=$2
 image=$3
 tag=$4
+waitOn=$5
 
 if [ ! -f kustomization.yaml ]; then 
 cat << EOF > kustomization.yaml
@@ -19,3 +20,7 @@ cat kustomization.yaml
 fi
 
 kubectl apply -k ./ --namespace $namespace
+
+if [ ! -z $waitOn ]; then
+kubectl rollout status $waitOn --namespace $namespace
+fi
