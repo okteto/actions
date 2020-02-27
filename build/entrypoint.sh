b88a7741776a974366afce5a725cbfaf11007bdd
Andreas <tullo@users.noreply.github.com>
2020-02-27 19:58:32 +0100

    Use optional build-args (#10)
    Signed-off-by: Andreas Amstutz <andreasamstutz@gmail.com>

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,8 +5,24 @@ token=$1
 tag=$2
 file=$3
 path=$4
+args=$5
+
+BUILDPARAMS=""
 
 export OKTETO_TOKEN=$token
-echo okteto build -t "$tag" -f "$file" "$path"
-okteto version
-okteto build -t "$tag" -f "$file" "$path"
+
+if [ ! -d ${HOME}/.okteto ]; then
+  mkdir ${HOME}/.okteto
+fi
+
+wget -O ${HOME}/.okteto/.ca.crt https://storage.googleapis.com/get.okteto.com/okteto-cloud-ca-crt
+
+if [ ! -z "${INPUT_BUILDARGS}" ]; then
+  for ARG in $(echo "${INPUT_BUILDARGS}" | tr ',' '\n'); do
+    BUILDPARAMS="${BUILDPARAMS} --build-arg ${ARG}=\'\$${ARG}\'"
+  done
+fi
+
+params=$(eval echo -t "$tag" -f "$file" "$BUILDPARAMS" "$path")
+echo $params
+okteto build $params
