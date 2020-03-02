43c43acf04031c69de583ac3076dd98eff6f9288
Andreas Amstutz <andreasamstutz@gmail.com>
2020-03-02 14:43:53 +0100

    Use okteto registry for pushing

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -5,22 +5,26 @@ namespace=$1
 manifest=$2
 full=$3
 waitOn=$4
+registry=$5
 
 image=$(echo $full | cut -d':' -f1)
 tag=$(echo $full | cut -d':' -f2)
+newName=$image
 
-if [ -z $image ]; then
+if [ -z $tag ]; then
 tag='latest'
 fi
 
-
-if [ ! -f kustomization.yaml ]; then 
+if [ ! -f kustomization.yaml ]; then
+  if [ ! -z $registry ]; then
+    newName=$(echo $registry/$image)
+  fi
 cat << EOF > kustomization.yaml
 resources:
  - $manifest
 images:
  - name: $image
-   newName: $image
+   newName: $newName
    newTag: $tag
 EOF
 cat kustomization.yaml
@@ -29,5 +33,5 @@ fi
 kubectl apply -k ./ --namespace $namespace
 
 if [ ! -z $waitOn ]; then
-kubectl rollout status $waitOn --namespace $namespace --timeout=300s
+  kubectl rollout status $waitOn --namespace $namespace --timeout=300s
 fi
