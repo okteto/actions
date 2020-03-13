e168e5cc85dc3d31628f4033cb5dc756c8a0ee7a
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-03-12 17:29:02 -0700

    include URL for okteto enterprise
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- login/entrypoint.sh
+++ login/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 token=$1
+url=$2
 if [ -z $token ]; then
   echo "Okteto API token is required"
   exit 1
 fi
 
-okteto login --token=$token
+echo running: okteto login --token=$token $url
+okteto login --token=$token $url
